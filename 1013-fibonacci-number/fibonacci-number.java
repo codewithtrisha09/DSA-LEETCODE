53fe8fa920fef31b55ec5712f3981d57993e71c9
Trisha Shetty <trishaScodes@gmail.com>
2026-07-04 18:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (33.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,15 +1,32 @@
-class Solution {
-    public int fib(int n) {
-        if(n==0){
-            return 0;
+
+class Solution{
+    public int fib(int n){
+        if(n<=1){
+            return n;
         }
-        if(n==1){
-            return 1;
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int current=a+b;
+            a=b;
+            b=current;
         }
-        if(n<0){
-            return 0;
-        }
-        return fib(n-1)+fib(n-2);
+        return b;
 
     }
 }
+// class Solution {
+//     public int fib(int n) {
+//         if(n==0){
+//             return 0;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+//         if(n<0){
+//             return 0;
+//         }
+//         return fib(n-1)+fib(n-2);
+
+//     }
+// }
