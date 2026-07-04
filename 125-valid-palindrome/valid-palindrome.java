97c623442ad245dfa5972fd57d786696ba117cca
Trisha Shetty <trishaScodes@gmail.com>
2026-07-04 18:38:59 +0530

    Time: 15 ms (20.69%) | Memory: 51.8 MB (5.24%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,24 +1,40 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         char[]chars=s.toCharArray();
+//         int left=0;
+//         int right=chars.length-1;
+//         while (left<right){
+//             if(!Character.isLetterOrDigit(chars[left])){
+//                 left++;
+//             }
+//             else if(!Character.isLetterOrDigit(chars[right])){
+//                 right--;
+//             }
+//             else{
+//                 if(Character.toLowerCase(chars[left])!=Character.toLowerCase(chars[right])){
+//                     return false;
+//                 }
+//                 left++;
+//                 right--;
+//             }
+            
+//         }
+//         return true;
+//     }
+// }
 class Solution {
     public boolean isPalindrome(String s) {
-        char[]chars=s.toCharArray();
-        int left=0;
-        int right=chars.length-1;
-        while (left<right){
-            if(!Character.isLetterOrDigit(chars[left])){
-                left++;
-            }
-            else if(!Character.isLetterOrDigit(chars[right])){
-                right--;
-            }
-            else{
-                if(Character.toLowerCase(chars[left])!=Character.toLowerCase(chars[right])){
-                    return false;
-                }
-                left++;
-                right--;
-            }
-            
+        String clean = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        return check(clean,0);
+    }
+    public boolean check(String s,int i){
+        int n=s.length();
+        if(i>=n/2){
+            return true;
         }
-        return true;
+        if(s.charAt(i)!=s.charAt(n-1-i)){
+            return false;
+        }
+        return check(s,i+1);
     }
 }
